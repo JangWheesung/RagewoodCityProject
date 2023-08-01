d6b4cc56dc72c6bfeab1832afedd288b79c12503
JangWheesung <gameplay1qaz@gmail.com>
2023-08-01 14:22:05 +0900

    Dash

--- Assets/03_Script/PlayerControl.cs
+++ Assets/03_Script/PlayerControl.cs
@@ -1,3 +1,4 @@
+using DG.Tweening.Core.Easing;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,8 +10,12 @@ public class PlayerControl : PlayerRoot
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float jumpCount;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
 
     float jumpCnt;
+    float dashLength = 0;
+    bool dashDelay;
 
     private void Start()
     {
@@ -24,18 +29,36 @@ public class PlayerControl : PlayerRoot
 
     private void Move()
     {
+        //양 옆 이동
         float x = Input.GetAxis("Horizontal") * moveSpeed;
-        rb.velocity = new Vector2(x, rb.velocity.y);
-
+        rb.velocity = new Vector2(x + dashLength, rb.velocity.y);
+        
+        //점프
         if (Input.GetKeyDown(KeyCode.Space) && jumpCnt > 0)
         {
             rb.velocity = new Vector2(x, 0.1f);
-            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
 
             jumpCnt--;
         }
 
         if (rb.velocity.y == 0)
             jumpCnt = jumpCount;
+
+        //대시
+        if (Input.GetKeyDown(KeyCode.LeftShift) && x != 0 && !dashDelay)
+        {
+            dashLength = Input.GetAxis("Horizontal") * dashSpeed;
+            StartCoroutine(DashDelay(dashTime));
+        }
+    }
+
+    private IEnumerator DashDelay(float time)
+    {
+        yield return new WaitForSeconds(0.1f);
+        dashLength = 0;
+        dashDelay = true;
+        yield return new WaitForSeconds(time);
+        dashDelay = false;
     }
 }

b03a4ade6475b6204d22b7f84f10aa1bd1a5b1a3
JangWheesung <gameplay1qaz@gmail.com>
2023-08-05 00:09:56 +0900

    Roulette1

--- Assets/03_Script/Manager/GaugeManager.cs
+++ Assets/03_Script/Manager/GaugeManager.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class GaugeManager : MonoBehaviour
 {
     public static GaugeManager instance;
 
     [SerializeField] private Image image;
+    [SerializeField] private RectTransform roulettePanel;
+    [SerializeField] private RectTransform rouletteImage;
+    [SerializeField] private Button rouletteBtn;
+    [SerializeField] private float rouletteTime;
     [SerializeField] private int[] maxGauge;
+
     private int gaugeLevel = 0;
+    private float spinSpeed;
 
     private void Awake()
     {
@@ -23,17 +30,30 @@ public class GaugeManager : MonoBehaviour
         {
             image.fillAmount = 0;
             gaugeLevel = maxGauge.Length - 1 <= gaugeLevel ? gaugeLevel : gaugeLevel + 1;
+            rouletteBtn.onClick.AddListener(RouletteSpin);
             RouletteOpen();
         }
     }
 
-    private void RouletteOpen()
+    private void Update()
     {
-
+        rouletteImage.Rotate(0, 0, spinSpeed);
     }
 
-    public void RouletteSpin()
+    private void RouletteOpen()
     {
+        roulettePanel.DOMoveY(550, 1f).SetEase(Ease.OutCirc);
+    }
 
+    private void RouletteSpin()
+    {
+        rouletteBtn.onClick.RemoveListener(RouletteSpin);
+        spinSpeed = 10f;
+    }
+
+    private IEnumerator stopSpin()
+    {
+        yield return new WaitForSeconds(rouletteTime);
+        //Mathf.Lerp();
     }
 }
